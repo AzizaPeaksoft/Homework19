0eabec0613434edbf8457b3d89a0ed6a23f783ed
aziza <azizaarzykulova>
2022-05-28 20:02:15 +0600

    Initial commit

---
+++ src/com/company/Car.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Car implements AutoCloseable{
+
+    @Override
+    public void close() {
+        System.out.println("Машина жабылып жатат ");
+
+    }
+    public void drive () {
+        System.out.println("Машина журуп жатат ");
+    }
+
+    public Car() {
+    }
+}
+
